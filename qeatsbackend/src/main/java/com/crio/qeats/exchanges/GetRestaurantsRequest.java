6d6ca14df8ffcfd84fd128539b2cffc8c32467fa
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2023-09-16 19:16:25 +0530

    mvc

---
+++ qeatsbackend/src/main/java/com/crio/qeats/exchanges/GetRestaurantsRequest.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.exchanges;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// TODO: CRIO_TASK_MODULE_RESTAURANTSAPI
+//  Implement GetRestaurantsRequest.
+//  Complete the class such that it is able to deserialize the incoming query params from
+//  REST API clients.
+//  For instance, if a REST client calls API
+//  /qeats/v1/restaurants?latitude=28.4900591&longitude=77.536386&searchFor=tamil,
+//  this class should be able to deserialize lat/long and optional searchFor from that.
+@Data
+
+
+public class GetRestaurantsRequest {
+    @NotNull
+    @Max(value = 90)
+    @Min(value = -90)
+    private final Double latitude;
+    @NotNull
+    @Max(value = 180)
+    @Min(value = -180)
+    private final Double longitude;
+    
+    private String searchFor;
+
+
+}
+
---
+++ qeatsbackend/src/main/java/com/crio/qeats/services/RestaurantServiceImpl.java
@@ -0,0 +1,128 @@
+
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+/* 
+package com.crio.qeats.services;
+
+import com.crio.qeats.dto.Restaurant;
+import com.crio.qeats.exchanges.GetRestaurantsRequest;
+import com.crio.qeats.exchanges.GetRestaurantsResponse;
+import com.crio.qeats.repositoryservices.RestaurantRepositoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class RestaurantServiceImpl implements RestaurantService {
+
+  private final Double peakHoursServingRadiusInKms = 3.0;
+  private final Double normalHoursServingRadiusInKms = 5.0;
+  @Autowired
+  private RestaurantRepositoryService restaurantRepositoryService;
+
+
+  // TODO: CRIO_TASK_MODULE_RESTAURANTSAPI - Implement findAllRestaurantsCloseby.
+  // Check RestaurantService.java file for the interface contract.
+  @Override
+  
+  public GetRestaurantsResponse findAllRestaurantsCloseBy(
+      GetRestaurantsRequest getRestaurantsRequest, LocalTime currentTime) {
+        LocalTime t1=LocalTime.of(07,59);
+        LocalTime t2=LocalTime.of(10,01);
+        LocalTime t3=LocalTime.of(12,59);
+        LocalTime t4=LocalTime.of(14,01);
+        LocalTime t5=LocalTime.of(18,59);
+        LocalTime t6=LocalTime.of(22,01);
+        //GetRestaurantsResponse[]
+        // res;
+        List<Restaurant>restaurants;
+        if(currentTime.isAfter(t1) && currentTime.isBefore(t2)){
+         restaurants= restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        }
+        else if(currentTime.isAfter(t3) && currentTime.isBefore(t4)){
+          restaurants= restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        }
+        else if(currentTime.isAfter(t5) && currentTime.isBefore(t6)){
+          restaurants= restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        }
+        else{//check
+          
+          restaurants= restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, normalHoursServingRadiusInKms);
+        }
+
+        return new GetRestaurantsResponse(restaurants);
+  }
+
+
+  shouhardik
+}
+
+*/
+
+/*
+ *
+ *  * Copyright (c) Crio.Do 2019. All rights reserved
+ *
+ */
+
+package com.crio.qeats.services;
+
+import com.crio.qeats.dto.Restaurant;
+import com.crio.qeats.exchanges.GetRestaurantsRequest;
+import com.crio.qeats.exchanges.GetRestaurantsResponse;
+import com.crio.qeats.repositoryservices.RestaurantRepositoryService;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class RestaurantServiceImpl implements RestaurantService {
+
+  private final Double peakHoursServingRadiusInKms = 3.0;
+  private final Double normalHoursServingRadiusInKms = 5.0;
+  @Autowired
+  private RestaurantRepositoryService restaurantRepositoryService;
+
+
+  // TODO: CRIO_TASK_MODULE_RESTAURANTSAPI - Implement findAllRestaurantsCloseby.
+  // Check RestaurantService.java file for the interface contract.
+  @Override
+  public GetRestaurantsResponse findAllRestaurantsCloseBy(
+      GetRestaurantsRequest getRestaurantsRequest, LocalTime currentTime) {
+        List<Restaurant> restaurants;
+        if(currentTime.isAfter(LocalTime.parse("07:59")) && currentTime.isBefore(LocalTime.parse("10:01"))) restaurants =  restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        else if(currentTime.isAfter(LocalTime.parse("12:59")) && currentTime.isBefore(LocalTime.parse("14:01"))) restaurants = restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        else if(currentTime.isAfter(LocalTime.parse("16:59")) && currentTime.isBefore(LocalTime.parse("21:01"))) restaurants = restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, peakHoursServingRadiusInKms);
+        else restaurants = restaurantRepositoryService.findAllRestaurantsCloseBy(getRestaurantsRequest.getLatitude(), getRestaurantsRequest.getLongitude(), currentTime, normalHoursServingRadiusInKms);
+        
+
+
+     return new GetRestaurantsResponse(restaurants);
+  }
+
+
+}
